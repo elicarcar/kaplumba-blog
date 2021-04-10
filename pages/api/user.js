8f8e489034dbe424826285530f46f4454c78e480
elicarcar <elisabetta.carcar@gmail.com>
2021-04-10 18:46:22 +0200

    update and delete user

--- pages/api/user.js
+++ pages/api/user.js
@@ -35,3 +35,58 @@ async function handleGetUsers(req, res) {
     res.status(500).send('Internal server error.');
   }
 }
+
+async function handleUpdateUser(req, res) {
+  try {
+    const { authorization } = req.headers;
+
+    const { username } = req.body;
+
+    if (!authorization) {
+      return res.status(401).send('Izin yok');
+    }
+
+    const { userId } = await jwt.verify(authorization, process.env.JWT_SECRET);
+
+    if (userId) {
+      const userUpdate = await User.findOneAndUpdate(
+        { _id: userId },
+        { $set: { username } },
+        { new: true }
+      );
+
+      res.status(201).json(userUpdate);
+    } else {
+      res.status(401).send('Bu eylemi yapmaya yetkiniz yok.');
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Internal server error.');
+  }
+}
+
+async function handleDeleteUser(req, res) {
+  try {
+    const { authorization } = req.headers;
+    if (!authorization) {
+      res
+        .status(401)
+        .send(
+          'Bu kullaniciyi silmeye yetkiniz yok. Lutfen yeniden giris yapin'
+        );
+    }
+
+    const { userId } = await jwt.verify(authorization, process.env.JWT_SECRET);
+
+    if (userId) {
+      await User.findOneAndDelete({ _id: userId });
+
+      res.status(204).send('Kullanici basarili bir sekilde silindi.');
+    } else {
+      res.status(404).send('Boyle bir kullanici yok.');
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Internal server error');
+  }
+}
