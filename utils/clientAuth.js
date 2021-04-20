eed453039ed188b2fba546b858f978bfbed0181e
elicarcar <elisabetta.carcar@gmail.com>
2021-04-20 19:59:58 +0200

    replace next router on clientAuth

--- utils/clientAuth.js
+++ utils/clientAuth.js
@@ -1,16 +1,17 @@
 import Router from 'next/router';
 import Cookies from 'js-cookie';
+import baseUrl from './baseUrl';
 
 export const setCookie = (token) => {
   Cookies.set('token', token.data);
-  Router.push('/profilim');
+  window.location.href = `${baseUrl}/profilim`;
 };
 
 export function redirectUser(ctx, location) {
   if (ctx.req) {
     // ctx.res.writeHead(302, { Location: location });
     // ctx.res.end();
-    Router.push(location);
+    window.location.href = `${baseUrl}/${location}`;
   }
 }
 
