438df82ea230afc1b1eecd077a6120e1571d0551
elicarcar <elisabetta.carcar@gmail.com>
2021-04-21 17:04:09 +0200

    try deploy on heroku

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://kaplumba-blog.vercel.app'
+    ? 'https://kaplumba.herokuapp.com'
     : 'http://localhost:3000';
 
 export default baseUrl;
