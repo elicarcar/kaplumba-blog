c55e2301eb4d384288c5c0877b006290d6773d26
Chilton <kat@eightarmcode.com>
2021-04-23 22:01:43 +0200

    kat: cors add headers, first run

--- next.config.js
+++ next.config.js
@@ -10,7 +10,12 @@ module.exports = {
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
+          // { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Origin', value: 'http://localhost:3000' },
+          { key: 'Origin', value: 'https://kaplumba-blog.vercel.app' },
+          { key: 'Accept', value: '*/*' },
+          { key: 'Referer', value: 'https://kaplumba-blog.vercel.app' },
+          { key: 'User-Agent', value: 'Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0' },
           {
             key: 'Access-Control-Allow-Methods',
             value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
